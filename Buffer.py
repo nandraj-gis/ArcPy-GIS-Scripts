69c2946c4a893729d4a9396f7e8f015ca8c8c843
nandraj-gis <surwasenandraj768@gmail.com>
2026-07-07 15:51:55 +0530

    Update Buffer.py

--- Buffer.py
+++ Buffer.py
@@ -1,8 +1,12 @@
 import arcpy
 
+# Input Feature Class
 input_fc = r"C:\GIS\Village.shp"
+
+# Output Feature Class
 output_fc = r"C:\GIS\Village_Buffer.shp"
 
+# Create 100 Meter Buffer
 arcpy.analysis.Buffer(
     input_fc,
     output_fc,
